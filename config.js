6324cccfbc151632004c2f982444ba275a691f5f
DominicDev <s0l0estudi4r@gmail.com>
2025-10-08 18:25:06 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HARDWELL';
+export let nameSongs = [
+	"SPACEMAN", /*1*/
+	"DARE YOU", /*2*/
+	"NEVER SAY GOODBYE", /*3*/
+	"ARCADIA", /*4*/
+	"COLORS", /*5*/
+	"SHINE A LIGHT", /*6*/
+	"OUT OF THIS TOWN", /*7*/
+	"THIS IS LOVE", /*8*/
+	"CALL ME A SPACEMAN", /*9*/
+	"APOLLO", /*10*/
+	"ECLIPSE", /*11*/
+	"YOUNG AGAIN", /*12*/
+	"RUN WILD", /*13*/
+	"ECHO", /*14*/
+	"HOW WE DO", /*15*/
+	"MAD WORLD", /*16*/
+	"POWER", /*17*/
+	"SUMMER AIR", /*18*/
+	"BIRDS FLY", /*19*/
+	"DON'T STOP THE MADNESS", /*20*/
+]
